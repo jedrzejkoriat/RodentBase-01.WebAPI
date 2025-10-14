82bc48da5d48330f680859441d791795bd444e18
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-10-14 19:10:43 +0200

    feat: add ratelimiter and swagger configurations

---
+++ RodentBase-01.WebAPI.API/Configuration/IServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using System.Threading.RateLimiting;
+using Microsoft.OpenApi.Models;
+
+namespace RodentBase_01.WebAPI.API.Configuration;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddCustomRateLimiter(this IServiceCollection services)
+    {
+        services.AddRateLimiter(options =>
+        {
+            options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+            {
+                var ip = httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+
+                return RateLimitPartition.GetFixedWindowLimiter(
+                    partitionKey: ip,
+                    factory: _ => new FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = 30,
+                        Window = TimeSpan.FromSeconds(10),
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                        QueueLimit = 0
+                    });
+            });
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorize",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+             });
+        });
+
+        return services;
+    }
+}
--- RodentBase-01.WebAPI.API/Program.cs
+++ RodentBase-01.WebAPI.API/Program.cs
@@ -1,8 +1,11 @@
+using RodentBase_01.WebAPI.API.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCustomRateLimiter();
 
 var app = builder.Build();
 
