e99b29c20ca72d0a106727f706ff8d2159f3c554
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-10-14 19:05:53 +0200

    feat: add exception handler middleware

---
+++ RodentBase-01.WebAPI.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace RodentBase_01.WebAPI.API.Middleware;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning(ex, "Exception", context.Request.Path);
+            await HandleExceptionAsync(context, HttpStatusCode.InternalServerError, "Internal Server Error", ex.Message);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, HttpStatusCode statusCode, string title, string details)
+    {
+        context.Response.ContentType = "application/problem+json";
+        context.Response.StatusCode = (int)statusCode;
+
+        var errorResponse = new
+        {
+            type = $"https://httpstatuses.com/{(int)statusCode}",
+            title,
+            status = (int)statusCode,
+            details,
+            instance = context.Request.Path
+        };
+
+        await context.Response.WriteAsJsonAsync(errorResponse);
+    }
+}
